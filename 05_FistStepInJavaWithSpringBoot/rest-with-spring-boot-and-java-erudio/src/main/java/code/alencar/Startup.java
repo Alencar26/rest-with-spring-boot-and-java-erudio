ed767442e8fab65c465ebef7db641bda77d4f74c
Andre Alencar <aaugusto310@gmail.com>
2023-09-08 04:06:42 -0300

    feat: First steps in Java with Spring Boot 3

---
+++ 05_FistStepInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/code/alencar/Startup.java
@@ -0,0 +1,13 @@
+package code.alencar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
