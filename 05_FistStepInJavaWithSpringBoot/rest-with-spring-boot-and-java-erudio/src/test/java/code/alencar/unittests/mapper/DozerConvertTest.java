07ac87f797def475f414abd38f734a695e3ea8da
Andre Alencar <aaugusto310@gmail.com>
2023-09-16 23:17:04 -0300

    test: Adicionando testes unitários para validar o Mapper que converte a um tipo de classe em outro.

---
+++ 05_FistStepInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/test/java/code/alencar/unittests/mapper/DozerConvertTest.java
@@ -0,0 +1,97 @@
+package code.alencar.unittests.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import code.alencar.data.vo.v1.PersonVO;
+import code.alencar.mapper.DozerMapper;
+import code.alencar.model.Person;
+import code.alencar.unittests.mocks.MockPerson;
+
+public class DozerConvertTest {
+    
+    MockPerson inputObject;
+
+    @BeforeEach
+    public void setUp() {
+        inputObject = new MockPerson();
+    }
+
+    @Test
+    public void parseEntityToVOTest() {
+        PersonVO person = DozerMapper.parseObject(inputObject.mockEntity(), PersonVO.class);
+        assertEquals(Long.valueOf(0), person.getId());
+        assertEquals("First Name0", person.getFirstName());
+        assertEquals("Last Name0", person.getLastName());
+        assertEquals("Address0", person.getAddress());
+        assertEquals("Male", person.getGender());
+    }
+
+
+    @Test
+    public void parseEntityListToVOListTest() {
+        List<PersonVO> personList = DozerMapper.parseListObject(inputObject.mockEntityList(), PersonVO.class);
+        PersonVO personZero = personList.get(0), 
+                 personSete = personList.get(7),
+                 personDozer = personList.get(12);
+
+        assertEquals(Long.valueOf(0), personZero.getId());
+        assertEquals("First Name0", personZero.getFirstName());
+        assertEquals("Last Name0", personZero.getLastName());
+        assertEquals("Address0", personZero.getAddress());
+        assertEquals("Male", personZero.getGender());
+
+        assertEquals(Long.valueOf(7), personSete.getId());
+        assertEquals("First Name7", personSete.getFirstName());
+        assertEquals("Last Name7", personSete.getLastName());
+        assertEquals("Address7", personSete.getAddress());
+        assertEquals("Female", personSete.getGender());
+
+        assertEquals(Long.valueOf(12), personDozer.getId());
+        assertEquals("First Name12", personDozer.getFirstName());
+        assertEquals("Last Name12", personDozer.getLastName());
+        assertEquals("Address12", personDozer.getAddress());
+        assertEquals("Male", personDozer.getGender());
+    }
+
+    @Test
+    public void parseVOToEntityTest() {
+        Person person = DozerMapper.parseObject(inputObject.mockVO(), Person.class);
+
+        assertEquals(Long.valueOf(0), person.getId());
+        assertEquals("First Name0", person.getFirstName());
+        assertEquals("Last Name0", person.getLastName());
+        assertEquals("Address0", person.getAddress());
+        assertEquals("Male", person.getGender());
+    }
+
+    @Test
+    public void parserVOListToEntityListTest() {
+        List<Person> personList = DozerMapper.parseListObject(inputObject.mockVOList(), Person.class);
+        Person personZero = personList.get(0), 
+                 personSete = personList.get(7),
+                 personDozer = personList.get(12);
+
+        assertEquals(Long.valueOf(0), personZero.getId());
+        assertEquals("First Name0", personZero.getFirstName());
+        assertEquals("Last Name0", personZero.getLastName());
+        assertEquals("Address0", personZero.getAddress());
+        assertEquals("Male", personZero.getGender());
+
+        assertEquals(Long.valueOf(7), personSete.getId());
+        assertEquals("First Name7", personSete.getFirstName());
+        assertEquals("Last Name7", personSete.getLastName());
+        assertEquals("Address7", personSete.getAddress());
+        assertEquals("Female", personSete.getGender());
+
+        assertEquals(Long.valueOf(12), personDozer.getId());
+        assertEquals("First Name12", personDozer.getFirstName());
+        assertEquals("Last Name12", personDozer.getLastName());
+        assertEquals("Address12", personDozer.getAddress());
+        assertEquals("Male", personDozer.getGender());
+    }
+}
---
+++ 05_FistStepInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/test/java/code/alencar/unittests/mocks/MockPerson.java
@@ -0,0 +1,54 @@
+package code.alencar.unittests.mocks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import code.alencar.data.vo.v1.PersonVO;
+import code.alencar.model.Person;
+
+public class MockPerson {
+    
+    public Person mockEntity() {
+        return mockEntity(0);
+    }
+
+    public PersonVO mockVO() {
+        return mockVO(0);
+    }
+
+    public List<Person> mockEntityList() {
+        List<Person> people = new ArrayList<Person>();
+        for (int i = 0; i < 14; i++) {
+            people.add(mockEntity(i));
+        }
+        return people;
+    }
+
+    public List<PersonVO> mockVOList() {
+        List<PersonVO> people = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            people.add(mockVO(i));
+        }
+        return people;
+    }
+
+    public Person mockEntity(Integer num) {
+        Person person = new Person();
+        person.setId(num.longValue());
+        person.setFirstName("First Name" + num);
+        person.setLastName("Last Name" + num);
+        person.setAddress("Address" + num);
+        person.setGender(((num % 2) == 0) ? "Male" : "Female");
+        return person;
+    }
+
+    public PersonVO mockVO(Integer num) {
+        PersonVO person = new PersonVO();
+        person.setId(num.longValue());
+        person.setFirstName("First Name" + num);
+        person.setLastName("Last Name" + num);
+        person.setAddress("Address" + num);
+        person.setGender(((num % 2) == 0) ? "Male" : "Female");
+        return person;
+    }
+}
