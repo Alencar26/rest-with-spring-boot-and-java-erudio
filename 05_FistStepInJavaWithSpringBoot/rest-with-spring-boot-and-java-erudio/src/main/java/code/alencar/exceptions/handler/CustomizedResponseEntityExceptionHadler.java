6f08aa379806ebf244e72ddd99458d060289c723
Andre Alencar <aaugusto310@gmail.com>
2023-11-27 07:04:27 -0300

    feat: add nova custom exception para objeto nulo.

--- 05_FistStepInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/code/alencar/exceptions/handler/CustomizedResponseEntityExceptionHadler.java
+++ 05_FistStepInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/code/alencar/exceptions/handler/CustomizedResponseEntityExceptionHadler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import code.alencar.exceptions.ExceptionResponse;
+import code.alencar.exceptions.RequiredObjectIsNullException;
 import code.alencar.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -35,5 +36,15 @@ public class CustomizedResponseEntityExceptionHadler extends ResponseEntityExcep
             request.getDescription(false));
 
         return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RequiredObjectIsNullException.class)
+    public final ResponseEntity<ExceptionResponse> handlerBadRequestException(Exception ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+            new Date(), 
+            ex.getMessage(), 
+            request.getDescription(false));
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
